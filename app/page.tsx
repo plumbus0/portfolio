12ef74618a50ca8669f505dcf65026d6f8048c83
Arya Prakash <arya.prakashyt@gmail.com>
2026-05-28 16:59:48 +1000

    made ui smaller for small screens

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
   
   return (  
     <div className="flex flex-col flex-1 items-center justify-center font-sans pointer-events-none">
-      <main className="flex flex-1 w-full max-w-6xl flex-col items-center justify-between py-10 px-16 sm:items-start">
+      <main className="flex flex-1 w-full max-w-6xl flex-col items-center justify-between sm:py-10 sm:px-16 p-0 sm:items-start">
         <div className="relative p-[1rem] w-[100%]">
         <Box>
           <div className="flex justify-between">
